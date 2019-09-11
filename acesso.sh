b0c6f6f74cf72e495f35e3a5ef7bd7599db78b33
Carlos Loyola <carlos.loyola@mandic.net.br>
2019-09-11 11:06:35 -0300

    incluido lista dos ids no scripts

--- acesso.sh
+++ acesso.sh
@@ -3,7 +3,13 @@ echo "Qual Profile vai utilizar?"
 read profile
 export AWS_PROFILE=$profile
 
-echo "Qual o ID da Instancia"
+echo "Ids encontrados no profile $profile"
+
+aws ssm describe-instance-information | grep InstanceId > instancias.txt 
+
+cat instancias.txt
+
+echo "Escolha o Id para acesso"
 read id
 
 echo "Qual a Região da Instancia"
