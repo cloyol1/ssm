e8b3329f46e336bfe5749ddd9c2bdbdc522d3bce
Carlos Loyola <carlos.loyola@mandic.net.br>
2019-09-12 13:02:38 -0300

    incluido escolha dos IDs por alfanumericos

--- acesso.sh
+++ acesso.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-echo "Qual Profile vai utilizar?"
+echo "Qual Profile vai utilizar ?"
 read profile
 export AWS_PROFILE=$profile
 
-echo "Qual a Região da Instancia"
+echo "Qual a Região da Instancia ?"
 read region
 
 aws ssm describe-instance-information --region $region | grep InstanceId > instancias.txt 
 
 if [[ $(wc -l instancias.txt | cut -d\  -f1 2> /dev/null) -gt 0 ]] 
 then
-	
   echo "Ids encontrados no profile $profile"
-	cat -n instancias.txt | awk '{print $1, $3}' | sed 's/"//g' | sed 's/,//g'
-	read -p "Escolha um ID: " id 
-	selecionado=`sed "$id" instancias.txt`
+	cat -n instancias.txt
+	read -p "Escolha o ID: " id 
+	selecionado=`sed "$id !d" instancias.txt | awk '{print $2}' | sed 's/"//g;s/,//g'`
 	echo "O ID escolhido é: " $selecionado
 	
 	aws ssm start-session --target $selecionado --region $region
