367c10c35facc29c47a0dfb01def489fb4324c5b
Eduardo Lisboa - Mandic <eduardo.lisboa@mandic.net.br>
2019-09-17 10:38:19 -0300

    - atualizado a partir do repositorio original

--- acesso.sh
+++ acesso.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-echo "Qual Profile vai utilizar?"
+echo "Qual Profile vai utilizar ?"
 read profile
 export AWS_PROFILE=$profile
 
-echo "Qual o ID da Instancia"
-read id
-
-echo "Qual a Região da Instancia"
+echo "Qual a Região da Instancia ?"
 read region
 
-aws ssm start-session --target $id --region $region
+aws ssm describe-instance-information --region $region | grep InstanceId > instancias.txt 
+
+if [[ $(wc -l instancias.txt | cut -d\  -f1 2> /dev/null) -gt 0 ]] 
+then
+  echo "Ids encontrados no profile $profile"
+	cat -n instancias.txt
+	read -p "Escolha o ID: " id 
+	selecionado=`sed "$id !d" instancias.txt | awk '{print $2}' | sed 's/"//g;s/,//g'`
+	echo "O ID escolhido é: " $selecionado
+	
+	aws ssm start-session --target $selecionado --region $region
+	
+else
+	
+	echo Nenhuma instancia encontrada na regiao $region
+	
+fi
