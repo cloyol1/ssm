b3cdf2577e455b2f4e5aaba1008e57918fc41996
Carlos Loyola <carlos.loyola@mandic.net.br>
2019-09-30 09:47:08 -0300

    nova versão da aplicação incluida, em andamento.

---
+++ versão02/acesso.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##### setar o profile e exporta para variavel de Ambiente #####
+echo "Qual Profile vai utilizar ?"
+read profile
+export AWS_PROFILE=$profile
+
+##### setar região onde estão as instancias #####
+echo "Qual a Região da Instancia ?"
+read region
+
+echo "Gerando lista de instancias gerenciaveis, aguarde... "
+
+ssm-session --list | awk '{print $1 " " $3}'  > teste.txt
+
+if [[ $(wc -l teste.txt | cut -d\  -f1 2> /dev/null) -gt 0 ]]
+then
+  echo "Ids encontrados no profile $profile"
+        cat -n teste.txt
+        read -p "Escolha o ID: " id
+        selecionado=`sed "$id !d" teste.txt | awk '{print $1}' # | sed 's/"//g;s/,//g'`
+        echo "O ID escolhido é: " $selecionado
+
+        aws ssm start-session --target $selecionado --region $region
+
+else
+
+        echo Nenhuma instancia encontrada na regiao $region
+
+fi
